d5faa6cbd6d8d6131b5bae1847b3e05e99f740af
Jocelyn-vnt <jocelyn.vinatie@etu.unilim.fr>
2024-10-15 10:08:59 +0200

    J-V Menu + Switch

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,44 @@
 import { Button } from "@/components/ui/button";
+import useFetch from "../public/useFetch.js";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Switch } from "@/components/ui/switch"
 
-export default function Home() {
+
+
+
+export default function App() {
+
+
+  const {
+    data: showData,
+    isLoading: isLoadingShow,
+    error: showError,
+  } = useFetch("/data.json")
   return (
-    <div> 
-      <Button>Click me</Button>
+    <div className="flex items-center place-content-between">
+      <DropdownMenu>
+        <DropdownMenuTrigger><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="bg-primary" className="size-10 text-primary" >
+          <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+        </svg>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <Switch />
 
     </div>
   );

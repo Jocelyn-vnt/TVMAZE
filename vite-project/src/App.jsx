d1136c89cd1306c9048a142d0594ca4363a787af
Jocelyn-vnt <jocelyn.vinatie@etu.unilim.fr>
2024-10-17 08:48:59 +0200

    JV - showData

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,6 @@
 import { Button } from "@/components/ui/button";
 import useFetch from "../public/useFetch.js";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { Switch } from "@/components/ui/switch"
+
 
 
 
@@ -21,25 +13,16 @@ export default function App() {
     isLoading: isLoadingShow,
     error: showError,
   } = useFetch("/data.json")
+  console.log(showData)
   return (
     <div className="flex items-center place-content-between">
-      <DropdownMenu>
-        <DropdownMenuTrigger><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="bg-primary" className="size-10 text-primary" >
-          <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-        </svg>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>Profile</DropdownMenuItem>
-          <DropdownMenuItem>Billing</DropdownMenuItem>
-          <DropdownMenuItem>Team</DropdownMenuItem>
-          <DropdownMenuItem>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-      <Switch />
-
+    { isLoadingShow ? showError
+      :
+      <div>
+        { showData && (<h2>{showData[0].show.name} {showData[0].score}</h2>)}
+      </div>
+      
+    }
     </div>
   );
 }
